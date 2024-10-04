74d867ea488312c86f5d96a22b1bc3407e5686e1
Elian Rene Huanca Choque <76572613+ElianHuanca@users.noreply.github.com>
2024-10-04 09:30:57 -0400

    Update const.js

--- src/const/const.js
+++ src/const/const.js
@@ -1,2 +1,3 @@
-export const baseUrl = "http://localhost:8080/api"; 
+//export const baseUrl = "http://localhost:8080/api"; 
 //export const baseUrl = "https://diagrammerdb-nodejs-production.up.railway.app/api";
+export const baseUrl = "https://diagramador-sw1-b51fd478951b.herokuapp.com/api";
